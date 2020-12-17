385883217f6a25ede75f0abec5f1b5fbc642a44e
Lovro <hodak.lovro@gmail.com>
2020-12-17 15:40:34 +0100

    front finished

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBarr from './components/NavBarr'
+import './App.css'
+
+export default function App() {
+    return (
+        <div>
+            <NavBarr />
+            <h1 className='test' >Here I am</h1>
+        </div>
+    )
+}
---
+++ client/src/config.js
@@ -0,0 +1,1 @@
+//export const API_URL = 'https://react-deploy-lovro.herokuapp.com/api'
